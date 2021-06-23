4904dd5f704e07c5b437d7ebfa1ac3562afe24b0
Ramy7729 <ramy7317@gmail.com>
2021-06-22 19:45:44 -0600

    Created a function get_users to reduce code duplication.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import dbconnect
 app = Flask(__name__)
 
 @app.post("/api/users")
-def create_user():
+def api_create_user():
     try:
         email = request.json['email']
         username = request.json['username']
@@ -61,37 +61,25 @@ def create_user():
     else: return Response("DB Error, Sorry!", mimetype="text/plain", status=500)
 
 @app.get("/api/users")
-def get_users():
+def api_get_users():
     try:
         user_id = int(request.args.get("userId", -1))
     except ValueError:
         return Response("userID must be a number.", mimetype="text/plain", status=400)
     
     if (user_id == -1):
-        users_properties = dbhelpers.run_select_statement("SELECT id, email, username, bio, birthdate, image_url, banner_url FROM user", [])
+        users = get_users("SELECT id, email, username, bio, birthdate, image_url, banner_url FROM user", [])
     else:
-        users_properties = dbhelpers.run_select_statement("SELECT id, email, username, bio, birthdate, image_url, banner_url FROM user WHERE id=?", [user_id])
+        users = get_users("SELECT id, email, username, bio, birthdate, image_url, banner_url FROM user WHERE id=?", [user_id])
     
-    if (users_properties == None):
+    if (users == None):
         return Response("Failed to GET users.", mimetype="text/plain", status=500)
-    results = []
-    for user_properties in users_properties:
-        user = {
-            "userId": user_properties[0],
-            "email": user_properties[1],
-            "username": user_properties[2],
-            "bio": user_properties[3],
-            "birthdate": user_properties[4],
-            "imageUrl": user_properties[5],
-            "bannerUrl":user_properties[6],
-        }
-        results.append(user)
 
-    users_json = json.dumps(results, default=str)
+    users_json = json.dumps(users, default=str)
     return Response(users_json, mimetype="application/json", status=200)
 
 @app.post("/api/login")
-def post_login():
+def api_post_login():
     try:  
         username = request.json['username']
         password = request.json['password']
@@ -125,5 +113,24 @@ def post_login():
     }
     login_json = json.dumps(logged_in_user, default=str)
     return Response(login_json, mimetype="application/json", status=201)
+
+def get_users(sql_statement, sql_params):
+    users_properties = dbhelpers.run_select_statement(sql_statement, sql_params)
+    
+    if (users_properties == None):
+        return None
+    users = []
+    for user_properties in users_properties:
+        user = {
+            "userId": user_properties[0],
+            "email": user_properties[1],
+            "username": user_properties[2],
+            "bio": user_properties[3],
+            "birthdate": user_properties[4],
+            "imageUrl": user_properties[5],
+            "bannerUrl":user_properties[6],
+        }
+        users.append(user)
+    return users
 
 app.run(debug=True)
