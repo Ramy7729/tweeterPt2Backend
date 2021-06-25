598cd54fc569c94d837c6211973eb86461df5622
Ramy7729 <ramy7317@gmail.com>
2021-06-25 00:04:18 -0600

    Added a GET method for the followers endpoint.

--- app.py
+++ app.py
@@ -211,6 +211,25 @@ def api_delete_follows():
     
     return Response("", mimetype="text/plain", status=204)
 
+@app.get("/api/followers")
+def api_get_followers():
+    try:
+        user_id = int(request.args["userId"])
+    except KeyError:
+        return Response("Please ensure a userId is sent", mimetype="text/plain", status=400)
+    except ValueError:
+        return Response("userID must be a number", mimetype="text/plain", status=400)
+    
+    users = get_users("SELECT f.user_id, email, username, bio, birthdate, image_url, banner_url FROM user u INNER JOIN follow f ON f.user_id = u.id WHERE f.follow_id=?", [user_id])
+    if (len(users) == 0):
+        users = get_users("SELECT id, email, username, bio, birthdate, image_url, banner_url FROM user u WHERE u.id=?", [user_id])
+        if (len(users) == 0):
+            return Response("User not found", mimetype="text/plain", status=404)
+        users = []
+
+    users_json = json.dumps(users, default=str) 
+    return Response(users_json, mimetype="application/json", status=200)
+
 def get_users(sql_statement, sql_params):
     users_properties = dbhelpers.run_select_statement(sql_statement, sql_params)
     
