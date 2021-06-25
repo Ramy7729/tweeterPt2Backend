9e7267b3dac515ccc502dafbf0ae3734808dd7ee
Ramy7729 <ramy7317@gmail.com>
2021-06-24 22:38:17 -0600

    Updated the api_get_follows method with an if statement for users that don't exist.

--- app.py
+++ app.py
@@ -164,13 +164,17 @@ def api_delete_login():
 def api_get_follows():
     try:
         user_id = int(request.args["userId"])
-        users = None
     except KeyError:
         return Response("Please ensure a userId is sent", mimetype="text/plain", status=400)
     except ValueError:
         return Response("userID must be a number", mimetype="text/plain", status=400)
     
     users = get_users("SELECT follow_id, email, username, bio, birthdate, image_url, banner_url FROM user u INNER JOIN follow f ON f.follow_id = u.id WHERE f.user_id=?", [user_id])
+    if (len(users) == 0):
+        users = get_users("SELECT id, email, username, bio, birthdate, image_url, banner_url FROM user u WHERE u.id=?", [user_id])
+        if (len(users) == 0):
+            return Response("User not found", mimetype="text/plain", status=404)
+        users = []
     
     users_json = json.dumps(users, default=str) 
     return Response(users_json, mimetype="application/json", status=200)
