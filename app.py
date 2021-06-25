5ec576b264f89dfd67614f198489a6709ae6390e
Ramy7729 <ramy7317@gmail.com>
2021-06-24 22:42:03 -0600

    Added a POST method for the follows endpoint.

--- app.py
+++ app.py
@@ -179,6 +179,25 @@ def api_get_follows():
     users_json = json.dumps(users, default=str) 
     return Response(users_json, mimetype="application/json", status=200)
 
+@app.post("/api/follows")
+def api_post_follows():
+    try: 
+        login_token = request.json['loginToken']
+        user_id = int(request.json["userId"])
+    except KeyError:
+        return Response("Please ensure all required fields are sent", mimetype="text/plain", status=400)
+    except ValueError:
+        return Response("userId must be a number", mimetype="text/plain", status=400)
+    
+    follow_user = dbhelpers.run_insert_statement("INSERT into follow(user_id, follow_id) SELECT user_session.user_id, ? FROM user_session WHERE user_session.token=?", [user_id, login_token])
+
+    if (follow_user == None):
+        users = get_users("SELECT id, email, username, bio, birthdate, image_url, banner_url FROM user u WHERE u.id=?", [user_id])
+        if (len(users) == 0):
+            return Response("User not found", mimetype="text/plain", status= 404)
+        return Response("User already followed.", mimetype="plain/text", status=500)
+    return Response("", mimetype="text/plain", status=204)
+
 def get_users(sql_statement, sql_params):
     users_properties = dbhelpers.run_select_statement(sql_statement, sql_params)
     
