534428ff7d26019d67d935b698d799f084174445
Ramy7729 <ramy7317@gmail.com>
2021-06-26 15:44:41 -0600

    Added a POST method for the tweet-likes endpoint.

--- tweet_likes.py
+++ tweet_likes.py
@@ -33,3 +33,28 @@ def api_get_tweet_likes():
         tweets_likes.append(tweets_likes_dictionary)
     tweets_likes_json = json.dumps(tweets_likes, default=str)
     return Response(tweets_likes_json, mimetype="application/json", status=200)
+
+@app.post("/api/tweet-likes")
+def api_post_tweet_likes():
+    try: 
+        login_token = request.json['loginToken']
+        tweet_id = int(request.json["tweetId"])
+    except KeyError:
+        return Response("Please ensure all required fields are sent", mimetype="text/plain", status=400)
+    except ValueError:
+        return Response("userId must be a number", mimetype="text/plain", status=400)
+    
+    tweet_like_id = dbhelpers.run_insert_statement("insert into tweet_like(user_id, tweet_id) select user_session.user_id, ? from user_session where user_session.token=?", [tweet_id, login_token])
+
+    if (tweet_like_id == None):
+        user_id = dbhelpers.run_select_statement("SELECT user_id FROM user_session us WHERE us.token =?", [login_token])
+        if (user_id == None):
+            return Response("Invalid loginToken", mimetype="plain/text", status=403)
+        tweet_id_info = dbhelpers.run_select_statement("SELECT id FROM tweet t WHERE t.id =?", [tweet_id])
+        if (tweet_id_info == None):
+            return Response("Tweet does not exist", mimetype="plain/text", status=404)
+        tweet_like = dbhelpers.run_select_statement("SELECT tl.id from tweet_like tl INNER JOIN user_session us on us.user_id = tl.user_id WHERE us.token=? and tl.tweet_id=?", [login_token, tweet_id])
+        if (len(tweet_like) == 1):
+            return Response("Tweet already liked", mimetype="plain/text", status=409)
+        return Response("Could not like the tweet.", mimetype="plain/text", status=500)
+    return Response("", mimetype="text/plain", status=201)
