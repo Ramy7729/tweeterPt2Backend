e1b6651cf568b14091e94665eefddf7da8cf1bf7
Ramy7729 <ramy7317@gmail.com>
2021-06-29 19:59:50 -0600

    Backend prep.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from application import app
+import sys
 
 import users
 import user_login
@@ -9,4 +10,19 @@ import tweet_likes
 import comments
 import comment_likes
 
-app.run(debug=True)
+if(len(sys.argv) > 1):
+    mode = sys.argv[1]
+else:
+    print("No mode argument, please pass a mode argument when invoking the file")
+    exit()
+
+if(mode == "production"):
+    import bjoern  # type: ignore
+    bjoern.run(app, "0.0.0.0", 5015)
+elif(mode == "testing"):
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
+else:
+    print("Invalid mode, please select either 'production' or 'testing'")
+    exit()
